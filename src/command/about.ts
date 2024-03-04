d898e63f2a62ae6ad1f89244d8b43df86f4af1c4
Andiputraw <andiputraw925@gmail.com>
2024-03-04 15:37:57 +0700

    fix: updated about command

--- src/command/about.ts
+++ src/command/about.ts
@@ -7,18 +7,8 @@ export const about: Command = {
     _payload: CommonPayload,
   ): CommandResponse {
     return new Embed().setTitle("About").setDescription(
-      `Hello. umm, i am here to help you to fetch some related document based on your need. feel free to do \`help\` to see what can i do for you`,
+      `Hello. umm, i am here to help you to fetch some related document based on your need. feel free to say \`help\` to see what can i do for you`,
     ).addField("github repositories", "https://github.com/andiputraw/hana-bot")
-      .addField(
-        "dependecies",
-        `
-- deno
-- deno std
-- deno-dom
-- tweetnacl
-- sift
-      `,
-      )
       .addField(
         "credits",
         `
