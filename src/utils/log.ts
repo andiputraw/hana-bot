a3d5d206557e534e8016a80424d0a1e040ddb64d
Andiputraw <andiputraw925@gmail.com>
2024-03-12 09:12:36 +0700

    feat: added runtime logging

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,11 +1,12 @@
 export const enum LogType {
   Info,
   Error,
+  Runtime,
 }
 // deno-lint-ignore no-explicit-any
 export function log(logType: LogType, ...args: any[]) {
   const isDebug = (Deno.env.get("DEBUG") || "true") === "true";
-  if (isDebug) {
+  if (isDebug || logType === LogType.Runtime) {
     let type;
     switch (logType) {
       case LogType.Info:
@@ -13,6 +14,9 @@ export function log(logType: LogType, ...args: any[]) {
         break;
       case LogType.Error:
         type = "[ERROR]: ";
+        break;
+      case LogType.Runtime:
+        type = "[RUNTIME]: ";
         break;
       default:
         type = "[UNKNOWN]";
